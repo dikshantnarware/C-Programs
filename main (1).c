d2eeed8d2b2e4f7d7b1e0e6443829e02a0627d51
Dikshant Narware <104089564+dikshantnarware@users.noreply.github.com>
2022-07-14 13:07:14 -0700

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,12 @@
+/* Escape Caracter Programs */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello\n World\ramshing\n"); //Escape Caracter;
+    printf("Dikshant\tNarware\n"); //namespace
+    printf("Ashwin\""); //DubbleQuATS;
+    
+    return 0;
+}
