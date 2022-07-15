a58916b8f0d4437a05858ffa358025e8a5aeeec3
Dikshant Narware <104089564+dikshantnarware@users.noreply.github.com>
2022-07-15 12:13:27 -0700

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,20 @@
+/* Swapping Programs */
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+    int x=10;
+    int y=20;
+    int t;
+    t=x;
+    x=y;
+   y=t;
+    
+    
+    printf("X=%i\n" , x); 
+    printf("Y=%i", y); 
+   
+     
+    return 0;
+}
---
+++ main (3).c
@@ -0,0 +1,21 @@
+/******
+
+Oprators Programs
+
+***********/
+#include <stdio.h>
+
+int main()
+{
+    
+    int x= 5;
+    int y= 7;
+  
+    printf("Plus=\t%i\n",x+y);
+    printf("Minus=\t%i\n",x-y);
+     printf("multiply=\t%i\n", x*y);
+      printf("Divided=\t%i\n",x/y);
+    printf("Percent=\t%i\n",x%y);
+
+    return 0;
+}
